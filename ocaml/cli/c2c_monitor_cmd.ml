f61a1a4e8e15c472108e79d6b73ec077944f5a8c
Max Kaye <m@xk.io>
2026-07-18 14:23:55 +1000

    Revert "Reapply "diag(B246): trace identity poll ticks + surface swallowed rebind exceptions (branch-only)""
    This reverts commit 87dce1f61100f5afb75da23f01b0cf2db70428b9.

--- ocaml/cli/c2c_monitor_cmd.ml
+++ ocaml/cli/c2c_monitor_cmd.ml
@@ -1287,15 +1287,7 @@ let monitor_cmd =
           let rec loop () =
             if Unix.getppid () = 1 then ()
             else begin
-              (* B246-DIAG: temporary poll-path tracing (diag branch only). *)
-              Printf.eprintf "B246-DIAG poll tick sid=%s reg_alias=%s cur=%s\n%!"
-                (match resolved_sid with Some s -> s | None -> "-")
-                (match session_reg_alias_now () with Some a -> a | None -> "-")
-                (match !my_alias_r with Some a -> a | None -> "-");
-              (try try_rebind_identity ()
-               with e ->
-                 Printf.eprintf "B246-DIAG poll exn: %s\n%!"
-                   (Printexc.to_string e));
+              (try try_rebind_identity () with _ -> ());
               Thread.delay 2.0;
               loop ()
             end
